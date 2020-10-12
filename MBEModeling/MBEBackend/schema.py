28ca8b48c446fe61d0b71e3eb88d1d2cc9615b11
MatthewAlmeida <matthew.almeida001@umb.edu>
2020-10-12 14:40:59 -0400

    Incremental dev

--- MBEModeling/MBEBackend/schema.py
+++ MBEModeling/MBEBackend/schema.py
@@ -6,7 +6,7 @@ from sqlalchemy import (
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
-    ForeignKey, relationship
+    relationship
 )
 from sqlalchemy.dialects.postgresql import JSONB
 
--- MBEModeling/lightning_module.py
+++ MBEModeling/lightning_module.py
@@ -7,7 +7,7 @@ from torch.nn import functional as F
 from torch.utils.data import DataLoader, random_split
 
 from torchvision.datasets import MNIST
-from torchvision import datasets, transforms
+from torchvision import transforms
 
 import pytorch_lightning as pl
 from pytorch_lightning.metrics.functional import accuracy
