8729d837b393d8254f3c5587f57e04dbc5e4bc77
MatthewAlmeida <matthew.almeida001@umb.edu>
2020-10-14 17:40:56 -0400

    CSV failover implementation checkpoint

--- src/postgres_logger.py
+++ src/postgres_logger.py
@@ -1,4 +1,6 @@
 import os
+import warnings
+from pathlib import Path
 
 from datetime import datetime
 from dotenv import (
@@ -7,7 +9,7 @@ from dotenv import (
 )
 
 from pytorch_lightning.loggers import (
-    LightningLoggerBase, rank_zero_experiment
+    LightningLoggerBase, CSVLogger
 )
 from pytorch_lightning.utilities import rank_zero_only
 
@@ -17,7 +19,7 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import sessionmaker
 
-from .schema import (
+from schema import (
     MBEBase, Analysis, Updates
 )
 
@@ -34,6 +36,7 @@ class PostgresLogger(LightningLoggerBase):
         self._version = experiment_version
         self._start_time = start_time
         self._epochs = epochs
+        self._failover = False
 
         # Set up database connection
 
@@ -50,40 +53,57 @@ class PostgresLogger(LightningLoggerBase):
         
         # Set up ORM
         self.db_engine = create_engine(self.connection_string)
-        MBEBase.metadata.create_all(self.db_engine)
 
-        # Initialize session
-        self.Session = sessionmaker(bind=self.db_engine)
+        # Here, the logger tries to hit the database for the
+        # first time. If it's not up, we failover into a CSV 
+        # logger and save the results so nothing is lost.
+        try:
+            MBEBase.metadata.create_all(self.db_engine)
 
-        # Create analysis object for this training run
-        self.analysis = Analysis(
-            name = self._name,
-            start_time = self._start_time,
-            epochs = self._epochs
-        )
+            # Initialize session
+            self.Session = sessionmaker(bind=self.db_engine)
 
+            # Create analysis object for this training run
+            self.analysis = Analysis(
+                name = self._name,
+                start_time = self._start_time,
+                epochs = self._epochs
+            )
+        except:
+            warnings.warn(
+                f"Unable to establish database connection. Failing "
+                f"over to CSV logging."
+            )
+            self._failover = True
+            log_dir = Path(f"/{os.getenv('LOG_DIR') or 'logs'}")
+            self.csv_logger = CSVLogger(log_dir, self._name, self._version)
 
     @rank_zero_only
     def log_hyperparams(self, params):
-        # Use "get" method so that any key misses
-        # will return None and leave those records
-        # Null.
-
-        self.analysis.learning_rate = params.get("Learning rate")
-        self.analysis.batch_size = params.get("Batch size")
-        self.analysis.train_fold_size = params.get("Train fold size")
+        if self._failover:
+            # Database unavailable, log to CSV.
+            self.csv_logger.log_hyperparams(params)
+        else:
+            # Use "get" method so that any key misses
+            # will return None and leave those records
+            # Null.
+            self.analysis.learning_rate = params.get("Learning rate")
+            self.analysis.batch_size = params.get("Batch size")
+            self.analysis.train_fold_size = params.get("Train fold size")
 
     @rank_zero_only
     def log_metrics(self, metrics, step):
+        
         return None
 
     @rank_zero_only
     def finalize(self, status):
         # Close all open connections
-        self.Session.close_all()
+        if not self.csv_logger:
+            self.Session.close_all()
 
     @property
-    @rank_zero_experiment
+    @rank_zero_only
     def experiment(self):
         return None
 
